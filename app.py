5be660edb9c81e9d146e265a9db6692d034d1a47
Andrews Medina <andrewsmedina@gmail.com>
2013-09-17 16:12:51 -0300

    getting port from environ.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+
+import os
+
+
 app = Flask(__name__)
 
 
@@ -8,4 +12,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=int(os.environ.get("PORT", "5000")))
